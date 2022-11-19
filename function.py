58e09574d01a5d5628e3ef1a4318ad382790382b
skwaweru254 <skwaweru254@gmail.com>
2022-11-19 11:08:34 +0300

    return none

--- function.py
+++ function.py
@@ -1,10 +1,14 @@
-
 def func(number=0):
     number += 90
     return number
+
+
 print(func(), func(8))
+
 
 def hello():
     print("print skwaweru254 and return none")
     return None
+
+
 hello()
